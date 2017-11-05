656c4c39c9c9dd265c22e9eb51d8f7420a38cfb8
Jack O'Connor <oconnor663@gmail.com>
2017-11-05 12:24:07 -0500

    copy sources before building

--- build.rs
+++ build.rs
@@ -1,36 +1,52 @@
-// File copied from libb2-sys.
+// Adapted from libb2-sys.
+
+extern crate walkdir;
 
 use std::path::*;
 use std::process::*;
 use std::env;
+use std::fs::{copy, create_dir_all};
 use std::io::*;
+use walkdir::WalkDir;
 
 fn main() {
-    let src = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("libb2");
+    let src_orig = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("libb2");
     let dst = PathBuf::from(&env::var("OUT_DIR").unwrap());
 
-    // `make` will automatically rerun `./configure` if the timestamps imply
-    // that it needs to.
-    if !src.join("Makefile").is_file() {
-        let mut configure_cmd = Command::new("./configure");
-        configure_cmd.current_dir(&src);
-        configure_cmd.arg("--prefix");
-        configure_cmd.arg(&dst);
-        // We're statically linking. Skip building the shared libs.
-        configure_cmd.arg("--enable-shared=no");
-        if env::var_os("CARGO_FEATURE_NATIVE").is_some() {
-            // This is the deafault for libb2, and we're just specifying it explicitly.
-            configure_cmd.arg("--enable-native=yes");
+    // Copy all the source files before running ./configure. That keeps Cargo
+    // from thinking files have "changed", so it doesn't redo the whole build
+    // every time.
+    let src_copy = dst.join("libb2_src");
+    println!("copying {:?} to {:?}", src_orig, src_copy);
+    for entry in WalkDir::new(&src_orig) {
+        let entry = entry.unwrap();
+        let entry_dest = src_copy.join(entry.path().strip_prefix(&src_orig).unwrap());
+        if entry.file_type().is_dir() {
+            println!("mkdir {:?}", entry_dest);
+            create_dir_all(entry_dest).unwrap();
         } else {
-            // The "fat library" figures out at runtime what x86 SIMD
-            // extensions it can use. Without this option, binaries build
-            // on new machines might not run if copied to older machines.
-            configure_cmd.arg("--enable-fat");
+            println!("copy {:?} {:?}", entry.path(), entry_dest);
+            copy(entry.path(), entry_dest).unwrap();
         }
-        run(&mut configure_cmd);
     }
-    run(Command::new("make").current_dir(&src));
-    run(Command::new("make").arg("install").current_dir(&src));
+
+    let mut configure_cmd = Command::new("./configure");
+    configure_cmd.current_dir(&src_copy);
+    configure_cmd.arg("--prefix");
+    configure_cmd.arg(&dst);
+    // We're statically linking. Skip building the shared libs.
+    configure_cmd.arg("--enable-shared=no");
+    if env::var_os("CARGO_FEATURE_NATIVE").is_some() {
+        // This is the deafault for libb2, and we're just specifying it explicitly.
+        configure_cmd.arg("--enable-native=yes");
+    } else {
+        // The "fat library" figures out at runtime what x86 SIMD
+        // extensions it can use. Without this option, binaries build
+        // on new machines might not run if copied to older machines.
+        configure_cmd.arg("--enable-fat");
+    }
+    run(&mut configure_cmd);
+    run(Command::new("make").arg("install").current_dir(&src_copy));
 
     println!("cargo:rustc-flags=-l static=b2");
     println!("cargo:rustc-flags=-L {}", dst.join("lib").display());
