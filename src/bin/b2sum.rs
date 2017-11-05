a58676bba458ae42d08a88d6afd14fb1fb2428b0
Jack O'Connor <oconnor663@gmail.com>
2017-11-05 11:27:59 -0500

    make b2sum buffer size more explicit

--- src/bin/b2sum.rs
+++ src/bin/b2sum.rs
@@ -3,10 +3,21 @@
 
 extern crate libb2;
 
-use std::io::{copy, stdin};
+use std::io::stdin;
+use std::io::prelude::*;
 
 fn main() {
+    let stdin = stdin();
+    let mut stdin_lock = stdin.lock();
     let mut state = libb2::blake2b::State::new(64);
-    copy(&mut stdin(), &mut state).unwrap();
+    // Using a big buffer like this is slightly more efficient than copy().
+    let mut buf = [0; 65536];
+    loop {
+        let n = stdin_lock.read(&mut buf).unwrap();
+        if n == 0 {
+            break;
+        }
+        state.update(&buf[..n]);
+    }
     println!("{}", state.finalize().hex());
 }
